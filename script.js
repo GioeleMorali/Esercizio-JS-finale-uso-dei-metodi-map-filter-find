5005a402a6bb119918eca5c7995593c0e051d518
lai2-01 <lai2-01@itismeucci.com>
2023-05-25 12:18:21 +0200

    primo commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const id = document.querySelector("#idAnimale")
+const idA = document.querySelector("#idAnimale")
 const nome = document.querySelector("#nome")
 const tipo = document.querySelector("#tipo")
 const castrato = document.querySelector("#castrato")
@@ -8,10 +8,20 @@ const tbody = document.querySelector("#tbody")
 const btnAdd = document.querySelector("#btnAdd");
 const btnMod = document.querySelector("#btnMod");
 
-const animali = []
+let  animali = []
 let counter = 1
 
 btnAdd.addEventListener('click', () => {
+    if(btnAdd.innerHTML == "Annulla modifica")
+    {
+        btnAdd.innerHTML = "Aggiungi"
+        btnMod.style.display = "none"
+        nome.value = ""
+        eta.value = ""
+        let buttons = tbody.querySelectorAll('button')
+        buttons.forEach(btn => btn.style.display = "inline")
+        return;
+    }
     if ( nome.value.trim().length < 3 ) {
         alert("Inserire il nome dell'animale")
         return false;
@@ -54,7 +64,8 @@ function render() {
 
 
 function elimina(id) {
-    // INSERISCI QUI IL CODICE
+    animali = animali.filter(animale => animale.id != id)
+    render()
 }
 
 function modifica(id) {
@@ -63,11 +74,43 @@ function modifica(id) {
     // Mostra nei campi della form i dati selezionati da modificare
     // Fai apparire il bottone "Registra Modifica" accanto al  bottone "Aggiungi"
     // Metti nel campo nascosto idAnimale il valore dell'id selezionato
+    let animale = animali.find(animale => animale.id == id)
+    idA.value = animale.id
+    nome.value = animale.nome
+    tipo.value = animale.tipo
+    castrato.value = animale.castrato
+    eta.value = animale.eta
+    btnMod.style.display = "inline"
+    btnAdd.innerHTML = "Annulla modifica"
+    let buttons = tbody.querySelectorAll('button')
+    buttons.forEach(btn => btn.style.display = "none")
 }
 
 // Scrivi il codice del bottone RegistraModifica
 btnMod.addEventListener('click', () => {
-    // Controlla se i campi nome ed eta sono valorizzati
+    if ( nome.value.trim().length < 3 ) {
+        alert("Inserire il nome dell'animale")
+        return false;
+    }
+    if ( eta.value.trim().length == 0 ) {
+        alert("Inserire l'età dell'animale")
+        return false;
+    }
+    animali.map(animale => {
+        if(animale.id == idA.value)
+        {
+            animale.id  = idA.value
+            animale.nome = nome.value
+            animale.tipo = tipo.value
+            animale.castrato = castrato.value
+            animale.eta = eta.value
+        }
+    })
+    render()
+    btnMod.style.display = "none"
+    btnAdd.innerHTML = "Aggiungi"
+    let buttons = tbody.querySelectorAll('button')
+    buttons.forEach(btn => btn.style.display = "inline")
     // modifica l'array animali con i dati modificati usando il metodo map()
     // Fai apparire i bottoni Elimina e Modifica
     // Fai sparire il bottone "Registra Modifica"
